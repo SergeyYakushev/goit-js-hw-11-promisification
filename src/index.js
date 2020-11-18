dfa1c0783593766a74304bc337113b90c4512d3d
SergeyYakushev <yakushev563@gmail.com>
2020-11-18 10:35:51 +0300

    Все 3 промисифицированны

--- src/index.js
+++ src/index.js
@@ -1,18 +1,104 @@
-//import menuCardTmpl from "./templates/example.hbs";
-//import cards from './example.json';
-import BSN from "bootstrap.native"
-import moment from 'moment';
-import './styles.css';
-
-const T = 1000;
-const myModalInstance = new BSN.Modal('.modal');
-const mod = document.querySelector('.modal');
-
-myModalInstance.show();
-
-mod.addEventListener('hide.bs.modal', openModal);
 
 
-function openModal() { 
-    setTimeout(()=>{myModalInstance.show()}, T)
-};
+ //import menuCardTmpl from "./templates/card_menu.hbs";
+//import cards from './menu.json'
+//import './styles.css';
+
+
+
+//Задача 1
+
+
+    const delay = ms => {
+        return new Promise ((resolve) =>{
+          setTimeout(()=>{
+               resolve(`${ms}`);}, ms);
+        });
+        };
+      
+      
+      const logger1 = time => console.log(`Resolved after ${time}ms`);
+      
+      // Вызовы функции для проверки
+      delay(2000).then(logger1); // Resolved after 2000ms
+      delay(1000).then(logger1); // Resolved after 1000ms
+      delay(1500).then(logger1); // Resolved after 1500ms
+
+
+
+// Задача 2
+
+      const users = [
+        { name: 'Mango', active: true },
+        { name: 'Poly', active: false },
+        { name: 'Ajax', active: true },
+        { name: 'Lux', active: false },
+      ];
+      
+      const toggleUserState = (allUsers, userName) => {
+
+        const updatedUsers = allUsers.map(user =>
+          user.name === userName ? { ...user, active: !user.active } : user,
+        );
+      
+       return Promise.resolve(updatedUsers);
+      };
+      
+      const logger = result => console.table(result);
+   
+      toggleUserState(users, 'Mango').then(logger);
+      toggleUserState(users, 'Lux').then(logger);
+
+
+// Задача 3
+
+      const randomIntegerFromInterval = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      };
+      
+      const makeTransaction = (transaction) => {
+        const delay = randomIntegerFromInterval(200, 500);
+      
+       return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            const canProcess = Math.random() > 0.3;
+        
+            if (canProcess) {
+
+                const arr = [transaction.id, delay ];
+            
+              resolve(arr);
+             
+            } else {
+              reject(transaction.id);
+            }
+          }, delay);
+           
+       });
+      };
+      
+      const logSuccess = ([id, time ]) => {
+        console.log(`Transaction ${id} processed in ${time} ms`);
+      };
+      
+      const logError = id => {
+        console.warn(`Error processing transaction ${id}. Please try again later.`);
+      };
+
+ 
+
+      makeTransaction({ id: 70, amount: 150 })
+      .then(logSuccess)
+      .catch(logError);
+    
+    makeTransaction({ id: 71, amount: 230 })
+      .then(logSuccess)
+      .catch(logError);
+    
+    makeTransaction({ id: 72, amount: 75 })
+      .then(logSuccess)
+      .catch(logError);
+    
+    makeTransaction({ id: 73, amount: 100 })
+      .then(logSuccess)
+      .catch(logError);
